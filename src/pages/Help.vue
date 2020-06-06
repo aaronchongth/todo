39e42d4a90f04eaf1fe4525a7af9abda6b66ab1d
Aaron Chong <aaronchongth@gmail.com>
2020-06-06 18:31:14 +0800

    Finished migrating all the tutorial assets

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere debitis voluptatibus harum nobis consectetur iusto molestias quibusdam tempore similique perferendis ullam totam vero consequatur sed magnam, dolor maiores saepe asperiores! Hic molestias ab ipsum blanditiis magnam minima, velit tenetur, necessitatibus sed doloremque omnis, voluptatum expedita exercitationem? Possimus nemo facere doloribus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis ullam quasi ad temporibus architecto, est natus! Tenetur aliquam ab officia! Cumque doloribus beatae ut dolorum nesciunt mollitia labore sapiente laborum similique dolorem molestias, nemo necessitatibus alias, ab magnam fugit accusamus ad ducimus quasi quod voluptatum in voluptatem. Consequuntur, mollitia!</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
